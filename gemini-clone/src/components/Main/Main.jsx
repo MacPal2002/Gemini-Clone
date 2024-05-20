4a8678eb41f6c9d651a62013a3d5cc8ddf1c6260
MacPal2002 <maciek.palenica@outlook.com>
2024-05-20 16:33:01 +0200

    connected inputs to gemmini api

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useContext } from 'react';
 import './Main.css'
 import { assets } from "../../assets/assets";
+import { Context } from '../../context/Context';
 
 const Main = () => {
+
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
+
   return (
     <div className='main'>
         <div className="nav">
@@ -10,39 +15,63 @@ const Main = () => {
             <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
-            <div className="greet">
-                <p><span>Cześć, Dev.</span></p>
-                <p>W czym mogę Ci pomóc?</p>
+            {!showResult
+            ?
+            <>
+                <div className="greet">
+                    <p><span>Cześć, Dev.</span></p>
+                    <p>W czym mogę Ci pomóc?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Zaproponuj piękne miejsca do zobaczenia podczas zbliżającej się podróży</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Krótko podsumuj tę koncepcję: urbanistyka</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Przeprowadź burzę mózgów dotyczącą działań łączących zespół w ramach naszych rekolekcji roboczych</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Popraw czytelność poniższego kodu</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>    
+            </>
+            : <div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?
+                    <div className="loader">
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    :
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                    }  
+                </div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Zaproponuj piękne miejsca do zobaczenia podczas zbliżającej się podróży</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Krótko podsumuj tę koncepcję: urbanistyka</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Przeprowadź burzę mózgów dotyczącą działań łączących zespół w ramach naszych rekolekcji roboczych</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Popraw czytelność poniższego kodu</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
+            }
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type="text" placeholder='Zadaj pytanie' />
+                    <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Zadaj pytanie' />
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={() => onSent()} src={assets.send_icon} alt="" />
                     </div>
                 </div>
                 <p className="bottom-info">
-                Informacje, które podaje Gemini (m.in. o osobach), mogą zawierać nieścisłości, dlatego dokładnie sprawdzaj odpowiedzi.
+                Informacje, które podaje Gemini (m.in. osobach), mogą zawierać nieścisłości, dlatego dokładnie sprawdzaj odpowiedzi.
                 </p>
             </div>
         </div>
